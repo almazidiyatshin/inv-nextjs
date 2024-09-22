5cf070b3605b5dbe864587100a2176dc962f484b
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-09-22 21:48:35 +0300

    fix imports

--- src/app/api/portfolio/route.ts
+++ src/app/api/portfolio/route.ts
@@ -1,4 +1,4 @@
-import { IPortfolioResponse } from '@/types/tInvest';
+import { IPortfolioResponse } from '@/config/api/types';
 import { NextResponse } from 'next/server';
 
 export async function POST() {
