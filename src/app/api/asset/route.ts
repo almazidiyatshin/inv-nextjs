ff8baead622deb11589d58bdb5c0a3dbf0626c82
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-09-22 21:43:02 +0300

    disable get asset api

--- src/app/api/asset/route.ts
+++ src/app/api/asset/route.ts
@@ -1,30 +1,30 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export async function POST({ dateFrom, dateTo, instrumentId }) {
-	const res = await fetch(
-		'https://invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetCandles',
-		{
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-				Authorization: 'Bearer ' + process.env.AUTH_TOKEN,
-			},
-			body: JSON.stringify({
-				from: dateFrom,
-				to: dateTo,
-				interval: 'CANDLE_INTERVAL_MONTH',
-				// instrumentId: 'TCS10A101X50',
-				instrumentId: instrumentId,
-				candleSourceType: 'CANDLE_SOURCE_UNSPECIFIED',
-				limit: 2,
-			}),
-		}
-	);
+// export async function POST({ dateFrom, dateTo, instrumentId }) {
+// 	const res = await fetch(
+// 		'https://invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetCandles',
+// 		{
+// 			method: 'POST',
+// 			headers: {
+// 				Accept: 'application/json',
+// 				'Content-Type': 'application/json',
+// 				Authorization: 'Bearer ' + process.env.AUTH_TOKEN,
+// 			},
+// 			body: JSON.stringify({
+// 				from: dateFrom,
+// 				to: dateTo,
+// 				interval: 'CANDLE_INTERVAL_MONTH',
+// 				// instrumentId: 'TCS10A101X50',
+// 				instrumentId: instrumentId,
+// 				candleSourceType: 'CANDLE_SOURCE_UNSPECIFIED',
+// 				limit: 2,
+// 			}),
+// 		}
+// 	);
 
-	if (!res.ok) throw new Error('Failed fetch response T-Invest');
+// 	if (!res.ok) throw new Error('Failed fetch response T-Invest');
 
-	const data = await res.json();
+// 	const data = await res.json();
 
-	return NextResponse.json(data);
-}
+// 	return NextResponse.json(data);
+// }
--- src/utils/getAssetData.ts
+++ src/utils/getAssetData.ts
@@ -1,17 +1,17 @@
-import { getFloatCost } from '../config/api/utils/common';
+// import { getFloatCost } from '../config/api/utils/common';
 
-export const getAssetData = async ({ dateFrom, dateTo, instrumentId }) => {
-	const request = await import('../app/api/asset/route');
-	const response = await (
-		await request.POST({ dateFrom, dateTo, instrumentId })
-	).json();
+// export const getAssetData = async ({ dateFrom, dateTo, instrumentId }) => {
+// 	const request = await import('../app/api/asset/route');
+// 	const response = await (
+// 		await request.POST({ dateFrom, dateTo, instrumentId })
+// 	).json();
 
-	const [candleFrom, candleTo] = response.candles;
+// 	const [candleFrom, candleTo] = response.candles;
 
-	const priceLastMonth = getFloatCost(
-		candleFrom.close.units,
-		candleFrom.close.nano
-	);
+// 	const priceLastMonth = getFloatCost(
+// 		candleFrom.close.units,
+// 		candleFrom.close.nano
+// 	);
 
-	return priceLastMonth;
-};
+// 	return priceLastMonth;
+// };
