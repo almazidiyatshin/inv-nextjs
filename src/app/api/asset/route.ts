c4b93002e4faa00592d522d53f3c582cced16e2c
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-09-22 21:45:05 +0300

    fix route

--- src/app/api/asset/route.ts
+++ src/app/api/asset/route.ts
@@ -1,30 +1,32 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export async function POST({ dateFrom, dateTo, instrumentId }) {
-// 	const res = await fetch(
-// 		'https://invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetCandles',
-// 		{
-// 			method: 'POST',
-// 			headers: {
-// 				Accept: 'application/json',
-// 				'Content-Type': 'application/json',
-// 				Authorization: 'Bearer ' + process.env.AUTH_TOKEN,
-// 			},
-// 			body: JSON.stringify({
-// 				from: dateFrom,
-// 				to: dateTo,
-// 				interval: 'CANDLE_INTERVAL_MONTH',
-// 				// instrumentId: 'TCS10A101X50',
-// 				instrumentId: instrumentId,
-// 				candleSourceType: 'CANDLE_SOURCE_UNSPECIFIED',
-// 				limit: 2,
-// 			}),
-// 		}
-// 	);
+type TParams = { dateFrom: string; dateTo: string; instrumentId: string };
 
-// 	if (!res.ok) throw new Error('Failed fetch response T-Invest');
+export async function POST({ dateFrom, dateTo, instrumentId }: TParams) {
+	const res = await fetch(
+		'https://invest-public-api.tinkoff.ru/rest/tinkoff.public.invest.api.contract.v1.MarketDataService/GetCandles',
+		{
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + process.env.AUTH_TOKEN,
+			},
+			body: JSON.stringify({
+				from: dateFrom,
+				to: dateTo,
+				interval: 'CANDLE_INTERVAL_MONTH',
+				// instrumentId: 'TCS10A101X50',
+				instrumentId: instrumentId,
+				candleSourceType: 'CANDLE_SOURCE_UNSPECIFIED',
+				limit: 2,
+			}),
+		}
+	);
 
-// 	const data = await res.json();
+	if (!res.ok) throw new Error('Failed fetch response T-Invest');
 
-// 	return NextResponse.json(data);
-// }
+	const data = await res.json();
+
+	return NextResponse.json(data);
+}
