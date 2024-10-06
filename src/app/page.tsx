1c4c9e6242b97180b7f7c0a7d1e64ff9634657e3
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-10-06 20:48:12 +0300

    fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Page() {
 
 	try {
 		const res = await fetch(`https://${process.env.VERCEL_URL}/api/indicators`);
+		console.log({ process }, process.env.VERCEL_URL);
 		indicatorsData = await res.json();
 	} catch (e) {
 		console.error(e);
