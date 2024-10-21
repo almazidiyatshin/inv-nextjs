67f5e7dab8c4c61343511738b0e4eaf9eb87e233
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2024-10-21 20:03:14 +0300

    fix candle cache politic

--- src/app/api/candles/route.ts
+++ src/app/api/candles/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest) {
 				candleSourceType: 'CANDLE_SOURCE_UNSPECIFIED',
 				limit: 12,
 			}),
-			cache: 'no-store',
+			cache: 'no-cache',
 		}
 	);
 
