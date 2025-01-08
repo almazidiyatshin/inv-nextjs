9edc028a637391d8ef80ff27ac92b2f8f5f81b42
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2025-01-08 20:55:20 +0300

    fix: fix auth config

--- src/config/auth/auth.ts
+++ src/config/auth/auth.ts
@@ -1,20 +1,23 @@
-import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 export const authOptions = {
 	providers: [
 		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
+			clientId: process.env.GITHUB_ID || '',
+			clientSecret: process.env.GITHUB_SECRET || '',
 		}),
 	],
 	callbacks: {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		async jwt({ token, user, account }) {
 			if (account && user) {
 				token.role = user.email === 'dev@tshin.ru' ? 'admin' : 'user';
 			}
 			return token;
 		},
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		async session({ session, token }) {
 			if (token) {
 				session.user.id = token.sub;
@@ -24,6 +27,3 @@ export const authOptions = {
 		},
 	},
 };
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
