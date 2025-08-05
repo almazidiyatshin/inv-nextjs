6a43681f9830bffd7d6c41fe18394d2d6aeb8214
Almaz Idiyatshin <anidiyatshin@dev.vtb.ru>
2025-08-05 20:23:39 +0300

    fix: exchange rate route was fixed

--- src/app/server/services/cbr.service.ts
+++ src/app/server/services/cbr.service.ts
@@ -1,15 +1,15 @@
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
-import { parseStringPromise } from 'xml2js';
-import type { TExchangeRateValuteItem, TIndicatorData } from '../types/cbr';
+import { parseStringPromise } from "xml2js";
+import type { TExchangeRateValuteItem, TIndicatorData } from "../types/cbr";
 
 export const getIndicators = async () => {
-	const response = await fetch('https://cbr.ru', {
-		cache: 'no-store',
+	const response = await fetch("https://cbr.ru", {
+		cache: "no-store",
 	});
 
 	if (!response.ok) {
-		throw new Error('Ошибка при получении индикаторов');
+		throw new Error("Ошибка при получении индикаторов");
 	}
 
 	const html = await response.text();
@@ -17,7 +17,7 @@ export const getIndicators = async () => {
 	const $ = cheerio.load(html);
 
 	const indicators: TIndicatorData[] = [];
-	$('div.main-indicator_value').each((_i, element) => {
+	$("div.main-indicator_value").each((_i, element) => {
 		const value = $(element).text().trim();
 		indicators.push({ value });
 	});
@@ -28,26 +28,26 @@ export const getIndicators = async () => {
 };
 
 export const getUsdExchangeRate = async () => {
-	const today = new Date().toLocaleDateString('ru-RU').replace(/\//g, '.');
+	const today = new Date().toLocaleDateString("ru-RU").replace(/\//g, ".");
 	const response = await fetch(
 		`https://www.cbr.ru/scripts/XML_daily.asp?date_req=${today}`,
 		{
-			cache: 'no-store',
-		}
+			cache: "no-store",
+		},
 	);
 
 	if (!response.ok) {
-		throw new Error('Ошибка при получении курса валют');
+		throw new Error("Ошибка при получении курса валют");
 	}
 
 	const xml = await response.text();
 
 	const parsed = await parseStringPromise(xml);
 	const list: TExchangeRateValuteItem[] = parsed.ValCurs.Valute;
-	const usd = list.find((v) => v.CharCode[0] === 'USD');
+	const usd = list.find((v) => v.CharCode[0] === "USD");
 
 	if (!usd) {
-		throw new Error('Ошибка при поиске курса usd');
+		throw new Error("Ошибка при поиске курса usd");
 	}
 
 	return usd.Value[0];
